3f1fed515c36af93928eeedee7b0bfe637621c25
ktiarts <51804341+ktiarts@users.noreply.github.com>
2022-01-29 18:36:20 -0500

    Add files via upload

---
+++ Libraries/Infrared/src/IrSequenceReader.h
@@ -0,0 +1,44 @@
+#ifndef IRSEQUENCEREADER_H
+#define	IRSEQUENCEREADER_H
+
+#include "IrSequence.h"
+#include "IrReader.h"
+
+/**
+ * This class packs an IrSequence into a dummy, immutable IrReader.
+ * It is basically intended for debugging and such.
+ */
+class IrSequenceReader : public IrReader {
+private:
+    IrSequence irSequence;
+
+public:
+    IrSequenceReader() : irSequence() {
+    };
+
+    IrSequenceReader(const IrSequenceReader& orig) : IrReader(),irSequence(orig.irSequence) {
+    };
+
+    IrSequenceReader(const IrSequence& irSequence_) : IrReader(),irSequence(irSequence_) {
+    };
+
+    virtual ~IrSequenceReader() {
+    };
+
+    void receive() {
+    };
+
+    bool isReady() const {
+        return true;
+    };
+
+    size_t getDataLength() const {
+        return irSequence.getLength();
+    };
+
+    microseconds_t getDuration(unsigned int index) const {
+        return irSequence.getDurations()[index];
+    };
+};
+
+#endif	/* IRSEQUENCEREADER_H */
