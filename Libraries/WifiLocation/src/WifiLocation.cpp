678f47ad4bb8b3f1b393ecc7775539beb64e15cf
ktiarts <51804341+ktiarts@users.noreply.github.com>
2022-01-29 20:44:57 -0500

    Add files via upload

---
+++ Libraries/WifiLocation/src/WifiLocation.cpp
@@ -0,0 +1,191 @@
+//
+//
+//
+
+#include "WifiLocation.h"
+
+const char* googleApisHost = "www.googleapis.com";
+const char* googleApiUrl = "/geolocation/v1/geolocate";
+
+#ifdef ARDUINO_ARCH_ESP32
+const char* googleCA = "-----BEGIN CERTIFICATE-----\n" \
+"MIIDujCCAqKgAwIBAgILBAAAAAABD4Ym5g0wDQYJKoZIhvcNAQEFBQAwTDEgMB4G\n" \
+"A1UECxMXR2xvYmFsU2lnbiBSb290IENBIC0gUjIxEzARBgNVBAoTCkdsb2JhbFNp\n" \
+"Z24xEzARBgNVBAMTCkdsb2JhbFNpZ24wHhcNMDYxMjE1MDgwMDAwWhcNMjExMjE1\n" \
+"MDgwMDAwWjBMMSAwHgYDVQQLExdHbG9iYWxTaWduIFJvb3QgQ0EgLSBSMjETMBEG\n" \
+"A1UEChMKR2xvYmFsU2lnbjETMBEGA1UEAxMKR2xvYmFsU2lnbjCCASIwDQYJKoZI\n" \
+"hvcNAQEBBQADggEPADCCAQoCggEBAKbPJA6+Lm8omUVCxKs+IVSbC9N/hHD6ErPL\n" \
+"v4dfxn+G07IwXNb9rfF73OX4YJYJkhD10FPe+3t+c4isUoh7SqbKSaZeqKeMWhG8\n" \
+"eoLrvozps6yWJQeXSpkqBy+0Hne/ig+1AnwblrjFuTosvNYSuetZfeLQBoZfXklq\n" \
+"tTleiDTsvHgMCJiEbKjNS7SgfQx5TfC4LcshytVsW33hoCmEofnTlEnLJGKRILzd\n" \
+"C9XZzPnqJworc5HGnRusyMvo4KD0L5CLTfuwNhv2GXqF4G3yYROIXJ/gkwpRl4pa\n" \
+"zq+r1feqCapgvdzZX99yqWATXgAByUr6P6TqBwMhAo6CygPCm48CAwEAAaOBnDCB\n" \
+"mTAOBgNVHQ8BAf8EBAMCAQYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUm+IH\n" \
+"V2ccHsBqBt5ZtJot39wZhi4wNgYDVR0fBC8wLTAroCmgJ4YlaHR0cDovL2NybC5n\n" \
+"bG9iYWxzaWduLm5ldC9yb290LXIyLmNybDAfBgNVHSMEGDAWgBSb4gdXZxwewGoG\n" \
+"3lm0mi3f3BmGLjANBgkqhkiG9w0BAQUFAAOCAQEAmYFThxxol4aR7OBKuEQLq4Gs\n" \
+"J0/WwbgcQ3izDJr86iw8bmEbTUsp9Z8FHSbBuOmDAGJFtqkIk7mpM0sYmsL4h4hO\n" \
+"291xNBrBVNpGP+DTKqttVCL1OmLNIG+6KYnX3ZHu01yiPqFbQfXf5WRDLenVOavS\n" \
+"ot+3i9DAgBkcRcAtjOj4LaR0VknFBbVPFd5uRHg5h6h+u/N5GJG79G+dwfCMNYxd\n" \
+"AfvDbbnvRG15RjF+Cv6pgsH/76tuIMRQyV+dTZsXjAzlAcmgQWpzU/qlULRuJQ/7\n" \
+"TBj0/VLZjmmx6BEP3ojY+x1J96relc8geMJgEtslQIxq/H5COEBkEveegeGTLg=="
+"-----END CERTIFICATE-----\n";
+#endif
+
+String WifiLocation::MACtoString(uint8_t* macAddress) {
+    uint8_t mac[6];
+    char macStr[18] = { 0 };
+#ifdef ARDUINO_ARCH_SAMD
+    sprintf(macStr, "%02X:%02X:%02X:%02X:%02X:%02X", macAddress[5], macAddress[4], macAddress[3], macAddress[2], macAddress[1], macAddress[0]);
+#elif defined ARDUINO_ARCH_ESP8266 || defined ARDUINO_ARCH_ESP32
+    sprintf(macStr, "%02X:%02X:%02X:%02X:%02X:%02X", macAddress[0], macAddress[1], macAddress[2], macAddress[3], macAddress[4], macAddress[5]);
+#endif
+    return  String(macStr);
+}
+
+WifiLocation::WifiLocation(String googleKey) {
+    _googleApiKey = googleKey;
+}
+
+// Function to get a list of surrounding WiFi signals in JSON format to get location via Google Location API
+String WifiLocation::getSurroundingWiFiJson() {
+    String wifiArray = "[\n";
+
+    int8_t numWifi = WiFi.scanNetworks();
+#ifdef DEBUG_WIFI_LOCATION
+    Serial.println(String(numWifi) + " WiFi networks found");
+#endif // DEBUG_WIFI_LOCATION
+    for (uint8_t i = 0; i < numWifi; i++) {//numWifi; i++) {
+      //Serial.print("WiFi.BSSID(i) = ");
+      //Serial.println((char *)WiFi.BSSID(i));
+#ifdef ARDUINO_ARCH_SAMD
+        byte bssid[6];
+
+        wifiArray += "{\"macAddress\":\"" + MACtoString(WiFi.BSSID(i,bssid)) + "\",";
+#elif defined ARDUINO_ARCH_ESP8266 || defined ARDUINO_ARCH_ESP32
+        wifiArray += "{\"macAddress\":\"" + MACtoString(WiFi.BSSID(i)) + "\",";
+#else
+#error Only ESP8266 and SAMD platforms are supported
+#endif
+        wifiArray += "\"signalStrength\":" + String(WiFi.RSSI(i)) + ",";
+        wifiArray += "\"channel\":" + String(WiFi.channel(i)) + "}";
+        if (i < (numWifi - 1)) {
+            wifiArray += ",\n";
+        }
+    }
+#if defined ARDUINO_ARCH_ESP8266 || defined ARDUINO_ARCH_ESP32
+    WiFi.scanDelete();
+#endif
+    wifiArray += "]";
+#ifdef DEBUG_WIFI_LOCATION
+    Serial.println("WiFi list :\n" + wifiArray);
+#endif // DEBUG_WIFI_LOCATION
+    return wifiArray;
+}
+
+// Calls Google Location API to get current location using surrounding WiFi signals inf
+location_t WifiLocation::getGeoFromWiFi() {
+
+    location_t location;
+    String response = "";
+#ifdef ARDUINO_ARCH_ESP32
+    _client.setCACert(googleCA);
+#endif
+    if (_client.connect(googleApisHost, 443)) {
+#ifdef DEBUG_WIFI_LOCATION
+        Serial.println("Connected to API endpoint");
+#endif // DEBUG_WIFI_LOCATION
+    } else {
+#ifdef DEBUG_WIFI_LOCATION
+        Serial.println("HTTPS error");
+#endif // DEBUG_WIFI_LOCATION
+        return location;
+    }
+
+    String body = "{\"wifiAccessPoints\":" + getSurroundingWiFiJson() + "}";
+#ifdef DEBUG_WIFI_LOCATION
+    Serial.println("requesting URL: " + String(googleApiUrl) + "?key=" + _googleApiKey);
+#endif // DEBUG_WIFI_LOCATION
+    String request = String("POST ") + String(googleApiUrl);
+    if (_googleApiKey != "")
+        request += "?key=" + _googleApiKey;
+    request += " HTTP/1.1\r\n";
+    request += "Host: " + String(googleApisHost) + "\r\n";
+    request += "User-Agent: ESP8266\r\n";
+    request += "Content-Type:application/json\r\n";
+    request += "Content-Length:" + String(body.length()) + "\r\n";
+    request += "Connection: close\r\n\r\n";
+    request += body;
+#ifdef DEBUG_WIFI_LOCATION
+    Serial.println("request: \n" + request);
+#endif // DEBUG_WIFI_LOCATION
+
+    _client.println(request);
+#ifdef DEBUG_WIFI_LOCATION
+    Serial.println("request sent");
+    Serial.print("Free heap: ");
+    Serial.println(ESP.getFreeHeap());
+#endif // DEBUG_WIFI_LOCATION
+    int timer = millis();
+
+    // Wait for response
+    while (millis() - timer < MAX_CONNECTION_TIMEOUT) {
+        if (_client.available())
+            break;
+        yield ();
+    }
+    while (_client.available()) {
+#ifdef DEBUG_WIFI_LOCATION
+        Serial.print(".");
+#endif // DEBUG_WIFI_LOCATION
+        response += _client.readString();
+        yield ();
+    }
+#ifdef DEBUG_WIFI_LOCATION
+    Serial.println();
+#endif // DEBUG_WIFI_LOCATION
+    if (response != "") {
+#ifdef DEBUG_WIFI_LOCATION
+        Serial.println("Got response:");
+        Serial.println(response);
+#endif // DEBUG_WIFI_LOCATION
+
+        int index = response.indexOf("\"lat\":");
+        if (index != -1) {
+            String tempStr = response.substring(index);
+            //Serial.println(tempStr);
+            tempStr = tempStr.substring(tempStr.indexOf(":") + 1, tempStr.indexOf(","));
+            //Serial.println(tempStr);
+            location.lat = tempStr.toFloat();
+#ifdef DEBUG_WIFI_LOCATION
+            Serial.println("\nLat: " + String(location.lat, 7));
+#endif // DEBUG_WIFI_LOCATION
+        }
+
+        index = response.indexOf("\"lng\":");
+        if (index != -1) {
+            String tempStr = response.substring(index);
+            //Serial.println(tempStr);
+            tempStr = tempStr.substring(tempStr.indexOf(":") + 1, tempStr.indexOf(","));
+            //Serial.println(tempStr);
+            location.lon = tempStr.toFloat();
+#ifdef DEBUG_WIFI_LOCATION
+            Serial.println("Lon: " + String(location.lon, 7));
+#endif // DEBUG_WIFI_LOCATION
+        }
+
+        index = response.indexOf("\"accuracy\":");
+        if (index != -1) {
+            String tempStr = response.substring(index);
+            //Serial.println(tempStr);
+            tempStr = tempStr.substring(tempStr.indexOf(":") + 1, tempStr.indexOf("\n"));
+            //Serial.println(tempStr);
+            location.accuracy = tempStr.toFloat();
+#ifdef DEBUG_WIFI_LOCATION
+            Serial.println("Accuracy: " + String(location.accuracy) + "\n");
+#endif // DEBUG_WIFI_LOCATION
+        }
+    }
+
+    return location;
+}
